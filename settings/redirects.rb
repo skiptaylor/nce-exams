9e1df360a84b0bd7a89ca3f7762d33fc6b10094c
Skip Taylor <skiptaylor101@gmail.com>
2020-02-21 07:20:32 -0500

    removes redirect

--- settings/redirects.rb
+++ settings/redirects.rb
@@ -1,14 +1,3 @@
-configure :production do
-	before do
-		
-    unless request.request_method == 'POST'
-  		unless request.url.include? "https://www."
-  			redirect "https://www.counselorexams.com#{request.path}"
-  		end
-    end
-		
-	end
-end
 
 ["/home/?", "/account/?"].each do |path|
   get path do
